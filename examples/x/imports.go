4df095b8ec2794bce7fe3b3b754b32047f53e4bc
thinh-wee <thinh@wee.vn>
2026-09-25 11:05:57 +0700

    Add example files for x package with go.mod, imports, and main functions

---
+++ examples/x/imports.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	_ "github.com/ioVN/pkg/x/crypto"
+	_ "github.com/ioVN/pkg/x/database"
+	_ "github.com/ioVN/pkg/x/io"
+	_ "github.com/ioVN/pkg/x/memory/mem"
+	_ "github.com/ioVN/pkg/x/net"
+)
+
+func imports() {
+}
---
+++ examples/x/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// Call the imports function to ensure all necessary packages are imported
+	imports()
+}
